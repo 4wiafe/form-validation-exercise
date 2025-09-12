d6e6eafcc92bba005733b18e1c5fc51c8e469bb4
Richmond Gyebi <gyebirichmond21@gmail.com>
2025-09-12 22:42:24 +0000

    Add html varibales to contain html elements

---
+++ script.js
@@ -0,0 +1,7 @@
+const form = document.querySelector(".form");
+const emailInput = document.querySelector("#email");
+const postalCode = document.querySelector("#postal-code");
+const country = document.querySelector("#country");
+const password = document.querySelector("#password");
+const confirmPassword = document.querySelector("#confirm-password");
+const errorMessages = document.querySelectorAll(".error");
