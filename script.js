c96bb23c4f735d0044e28f7ed0b9257a0df316c1
Richmond Gyebi <gyebirichmond21@gmail.com>
2025-09-13 11:13:06 +0000

    Add emailvalidation functionality

--- script.js
+++ script.js
@@ -5,3 +5,25 @@ const country = document.querySelector("#country");
 const password = document.querySelector("#password");
 const confirmPassword = document.querySelector("#confirm-password");
 const errorMessages = document.querySelectorAll(".error");
+
+function showEmailError() {
+  let message = errorMessages[0];
+
+  if (emailInput.validity.typeMismatch) {
+    message.textContent = "Invalid email address.";
+    message.classList.add("active");
+  } else if (emailInput.validity.valueMissing) {
+    message.textContent = "Enter an email address.";
+    message.classList.add("active");
+  } else {
+    message.classList.remove("active");
+  }
+}
+
+emailInput.addEventListener("input", showEmailError);
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  showEmailError();
+});
